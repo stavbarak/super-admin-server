b35caa6d3f46fa3e7889415740823d2229bfadd0
Stav Barak <stavb@tikalk.com>
2019-11-30 23:13:13 +0200

    authorized request

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+const tokenForUser = (user) => {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = (req, res, next) => {
     const email = req.body.email;
@@ -21,7 +28,7 @@ exports.signup = (req, res, next) => {
     user.save((err) => {
         if(err) { return next(err); }
 
-        res.json({ success: true });
+        res.json({ token: tokenForUser(user) });
     });
 
 }
